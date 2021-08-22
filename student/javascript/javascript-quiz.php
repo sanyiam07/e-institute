e7c51e583caddb0b62522f5f652e9948b0def1e7
sanyiam07 <89332850+sanyiam07@users.noreply.github.com>
2021-08-23 00:19:41 +0530

    Add files via upload

---
+++ student/javascript/javascript-quiz.php
@@ -0,0 +1,48 @@
+
+<html>
+<head>
+	<title>Javascript Quizer</title>
+	<link rel="stylesheet" type="text/css" href="../css/1css.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<p>Javascript Quizer</p>
+		</div>
+	</header>
+
+	<main>
+		
+		
+			<div class="container">
+				<h2>Test Your Javascript Knowledge</h2>
+				<p>
+					This is a multiple choise quiz to test your Javascript Knowledge.
+				</p>
+				<ul>
+					<li><strong>Number of Questions:</strong>10 </li>
+					<li><strong>Type:</strong> Multiple Choise</li>
+					<li><strong>Estimated Time:</strong> 30 min</li>
+
+				</ul>
+
+				<a href="javascript-question.php" class="start">Start Quiz</a>
+			
+				
+				
+			</div>
+
+		
+	</main>
+
+
+	<footer>
+			<div class="container">
+				Copyright &copy; NAVIGUS
+			</div>
+
+
+	</footer>
+
+	
