12204d093b3ad2b25b4572d2d1d7707fea0725fb
sanyiam07 <89332850+sanyiam07@users.noreply.github.com>
2021-08-23 00:16:54 +0530

    Add files via upload

---
+++ student/c++/c++-question.php
@@ -0,0 +1,190 @@
+
+<html>
+<head>
+	<title>C++ Quizer</title>
+	<link rel="stylesheet" type="text/css" href="../css/1css.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<p>C++ Quizer</p>
+		</div>
+	</header>
+
+	<main>
+			<div class="container">
+			<div class="current">Question 1 of 10  </div>
+				<p class="question">what is full form of Html ? </p>
+				<form method="POST" action="../thanks.php">
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 2 of 10  </div>
+				<p class="question">what is full form of Html ? </p>
+				
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 3 of 10  </div>
+				<p class="question">what is full form of Html ? </p>
+				<form method="POST" action="#">
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 4 of 10  </div>
+				<p class="question">what is full form of Html ? </p>
+				
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 5 of 10  </div>
+				<p class="question">what is full form of Html ?  </p>
+			
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 6 of 10  </div>
+				<p class="question">what is full form of Html ?  </p>
+				
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 7 of 10  </div>
+				<p class="question">what is full form of Html ?  </p>
+			
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 8 of 10  </div>
+				<p class="question">what is full form of Html ?  </p>
+				
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 9 of 10  </div>
+				<p class="question">what is full form of Html ? </p>
+			
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					<div class="current">Question 10 of 10  </div>
+				<p class="question">what is full form of Html ?> </p>
+				
+				<ul class="choicess">
+					
+						<li><input type="radio" name="choice" value="1">hypeww</li>
+						<li><input type="radio" name="choice" value="2">hypeww</li>
+						<li><input type="radio" name="choice" value="3">hypeww</li>
+						<li><input type="radio" name="choice" value="4">hypeww</li>
+
+						
+						
+						
+					</ul>
+					
+					
+					
+
+					
+					&nbsp; &nbsp; <input type="submit" name="submit" value="Submit">
+
+
+				</form>
+				
+
+			</div>
+
+	</main>
+
+
+	<footer>
+			<div class="container">
+				Copyright &copy; NAVIGUS
+			</div>
+
+
+	</footer>
+
+
+
+
+
+
+
+
+</body>
+</html>
+
---
+++ student/c++/c++-quiz.php
@@ -0,0 +1,48 @@
+
+<html>
+<head>
+	<title>C++ Quizer</title>
+	<link rel="stylesheet" type="text/css" href="../css/1css.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<p>C++ Quizer</p>
+		</div>
+	</header>
+
+	<main>
+		
+		
+			<div class="container">
+				<h2>Test Your C++ Knowledge</h2>
+				<p>
+					This is a multiple choise quiz to test your C++ Knowledge.
+				</p>
+				<ul>
+					<li><strong>Number of Questions:</strong>10 </li>
+					<li><strong>Type:</strong> Multiple Choise</li>
+					<li><strong>Estimated Time:</strong> 30 min</li>
+
+				</ul>
+
+				<a href="c++-question.php" class="start">Start Quiz</a>
+			
+				
+				
+			</div>
+
+		
+	</main>
+
+
+	<footer>
+			<div class="container">
+				Copyright &copy; NAVIGUS
+			</div>
+
+
+	</footer>
+
+	
