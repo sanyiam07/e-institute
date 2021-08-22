fe78511dd09ced6c39e54cf815855531875516b4
sanyiam07 <89332850+sanyiam07@users.noreply.github.com>
2021-08-23 00:19:09 +0530

    Add files via upload

---
+++ student/java/java-quiz.php
@@ -0,0 +1,48 @@
+
+<html>
+<head>
+	<title>Java Quizer</title>
+	<link rel="stylesheet" type="text/css" href="../css/1css.css">
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<p>Java Quizer</p>
+		</div>
+	</header>
+
+	<main>
+		
+		
+			<div class="container">
+				<h2>Test Your Java Knowledge</h2>
+				<p>
+					This is a multiple choise quiz to test your Java Knowledge.
+				</p>
+				<ul>
+					<li><strong>Number of Questions:</strong>10 </li>
+					<li><strong>Type:</strong> Multiple Choise</li>
+					<li><strong>Estimated Time:</strong> 30 min</li>
+
+				</ul>
+
+				<a href="java-question.php" class="start">Start Quiz</a>
+			
+				
+				
+			</div>
+
+		
+	</main>
+
+
+	<footer>
+			<div class="container">
+				Copyright &copy; NAVIGUS
+			</div>
+
+
+	</footer>
+
+	
